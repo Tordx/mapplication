112d1181288d353dde63ea4f36760e1c3727746e
Roven Kyle Narra <ellamateogang@gmail.com>
2023-04-09 13:58:20 +0800

    userview fixed

--- code/screens/UserViewPage.jsx
+++ code/screens/UserViewPage.jsx
@@ -1,7 +1,9 @@
 import { View, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
-import PouchDB from 'pouchdb-react-native' ; 'pouchdb-core';
+import PouchDB from 'pouchdb-react-native' ;import { Image } from 'react-native';
+import { Black, White } from '../Assets/Colors/Colors';
+ 'pouchdb-core';
 export default function UserViewPage() {
 
     const {userview} = useSelector((action) => action.user)
@@ -13,47 +15,45 @@ export default function UserViewPage() {
 
 
     const getdata = async () => {
-        const dbremoteAccounts = new PouchDB('http://admin:admin@192.168.0.192:5984/m_account');
-        
+      const dbremoteAccounts = new PouchDB('http://admin:admin@192.168.0.192:5984/m_account');
+     
         try {
-
           let result = await dbremoteAccounts.allDocs({
             include_docs: true,
             attachments: true,
-            
           });
-          if(result.rows){
-            let modifiedArr = result.rows.map(item => 
-                item.doc
-            )
+          if (result.rows) {
+            let modifiedArr = result.rows.map(
+              item => item.doc
+            );
             let filteredData = modifiedArr.filter(item => {
-              return item
-            })
-            if (!filteredData) {
-                let newFilteredData = filteredData.filter((item) => {
-                  return item.userID === userview.userID;
-                })
-
-                const object = newFilteredData[0]
-                console.log('loaded');
-                setData(object);
-                console.log('loaded');
-                console.log(object);
-                
-                
+              return item.UserID === userview.UserID
+            });
+            if (filteredData.length) {
+              let newFilterData = filteredData.map((item) => {
+                return item
+              });
+              const FullDetails = newFilterData[0]
+              const adminusername = newFilterData[0].username
+              const adminpassword = newFilterData[0].password
+              
+              setData(FullDetails)
             }
-          } 
+          }
         } catch (error) {
           console.error(error);
+          console.log(error)
         }
-    }
-    getdata
+      }
+    
+    getdata()
     console.log('loaded');
     },[])
   return (
-    <View style ={{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center',}}>
-        <Text>{data.FullName}</Text>
-     
+    <View style ={{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: Black}}>
+         <Image style = {{width: 200, height: 200, borderRadius: 100, marginVertical: 20}} resizeMode='contain' source={{uri: data.Image}} />
+         <Text style = {{ fontSize: 30, color: White, fontFamily: 'Nexa-ExtraLight', justifyContent: 'center', alignItems: 'center', textAlign: 'left', marginBottom: 10}}>{data?.FullName}</Text> 
+         <Text style={{fontSize: 20, color: White, textAlign: 'center', alignSelf: 'center', fontFamily: 'Nexa-ExtraLight'}}>Reviews: {data.CommentCount}</Text>
     </View>
   )
 }
