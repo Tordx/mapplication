10918ed647c93a8088bc48ad3a95300e49ef7c11
Roven Kyle Narra <ellamateogang@gmail.com>
2023-04-10 03:06:13 +0800

    fixed signup, added custom css and more

--- code/screens/signup.jsx
+++ code/screens/signup.jsx
@@ -1,6 +1,6 @@
 
-import React, {useState} from 'react';
-import {View, Text, Image, TextInput, Pressable, KeyboardAvoidingView , Alert , Button , Platform, ScrollView } from 'react-native'
+import React, {useEffect, useState} from 'react';
+import {View, Text, Image, TextInput, Pressable, KeyboardAvoidingView , Alert , Button , Platform, ScrollView, ToastAndroid, BackHandler } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useNavigation } from '@react-navigation/native';
 import { dbremoteAccounts } from '../../database/database';
@@ -12,8 +12,20 @@ import { Loginbox } from './loginPage';
 import { Black, LightBlue, LightYellow, White } from '../Assets/Colors/Colors';
 import { StyleSheet } from 'react-native';
 
+const handleBackButton = () => {
+  return true;
+};
 
 const Signup = () => {
+
+    useEffect(() => {
+      BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+  
+      return () => {
+        BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+      };
+    }, []);
+  
 
   const [show, setShow] = useState(false)
   const [username, setUsername] = useState('')
@@ -21,7 +33,7 @@ const Signup = () => {
   const [MobileNumber, setMobileNumber] = useState('')
   const [Nationality, setNationality] = useState('')
   const [IDType, setIDType] = useState('')
-  const [IDCardImage, setIDCardImage] = useState('')
+  const [disability, setDisability] = useState('')
   const [IDNumber, setIDNumber] = useState('')
   const [ProfilePicture, setProfilePicture] = useState('')
   const [FirstName, setFirstName] = useState('')
@@ -86,6 +98,7 @@ const Signup = () => {
           const photolink = result.data.link
           setIdCardImage(photolink)
           console.log('photolink', photolink);
+          ToastAndroid.show('Sucessfully added your ID', ToastAndroid.LONG)
         } catch (error) {
           console.log('error', error);
         }
@@ -93,17 +106,16 @@ const Signup = () => {
     });
   };
 
-  const setNewSuperAdmin = async () => {
+  const setnewuser = async () => {
       
        try {
-         var NewSuperAdmin = {
+         var adduser = {
              _id: id,
              username : username,
              password : password,
              MobileNumber : MobileNumber,
              Nationality : Nationality,
-             IDType : IDType,
-             IDCardImage : IDCardImage,
+             Disability : disability,
              IDNumber : IDNumber,
              ProfilePicture : ProfilePicture,
              UserID: id,
@@ -115,10 +127,11 @@ const Signup = () => {
              Sex : Sex,
              Address : Address,
              AlternateContactNumber : AlternateContactNumber,
-             Profilephoto: Profilephoto,
-             Idcardimage: Idcardimage
+             Image: Profilephoto,
+             Idcardimage: Idcardimage,
+             Active: 'inactive'
          }
-      dbremoteAccounts.put(NewSuperAdmin)
+      dbremoteAccounts.put(adduser)
          .then((response) =>{
            Alert.alert('Your Super Admin is Added has been successfully added!')
            console.log(response)
@@ -139,15 +152,44 @@ const Signup = () => {
       const [third, setthird] = useState(false)
       const [forth, setForth] = useState(false)
       const [fifth, setfifth] = useState(false)
+
+      const handleNoSpace = (inputText) => {
+        // Remove spaces from the input text using a regular expression
+        const nospace = inputText.replace(/\s/g, '');
+        setUsername(nospace)
+      };
+
+      const handlefirst = () => {
+        if ((username.length < 6) && (password.length < 6)) {
+          ToastAndroid.show('username and password must contain at least 6 alphanumeric', ToastAndroid.CENTER)
+        } else {
+          setFirst(false); setSecond(true) 
+        }
+      }
+      const handleSecond = () => {
+        if (!(FirstName && LastName && Address && Birthday && Sex)) {
+          ToastAndroid.show('Please fill up all fields', ToastAndroid.SHORT)
+        } else {
+          setSecond(false); setthird(true)
+        }
+      }
+
+      const handleFourth = () => {
+        if (!(Nationality && disability && IDNumber && Idcardimage)) {
+          ToastAndroid.show('Please fill up all fields', ToastAndroid.SHORT)
+        } else {
+          setForth(false); setfifth(true)
+        }
+      }
   return (
-    <View style  ={{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: Black}} behavior='padding'>
+    <View style  ={{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: Black}} >
       <ScrollView style = {{width: '100%', height: '100%'}}>
-      <View style = {{justifyContent: 'center', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'}}>
+      <KeyboardAvoidingView style = {{justifyContent: 'center', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'}}>
        {opening && 
         <View style = {{justifyContent: 'center', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'}}>
        <Image source = {require('../Assets/images/welcome-signup.png')} style = {{width: 1000, height: 300}} resizeMode = 'contain' />
         <Text style = {styles.headertagline}>Welcome to Alaminos city PWD community!</Text>
-        <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20}}>We're thrilled to have you on our journey towards a more inclusive world for people with disabilities. With your support, we can work towards breaking down barriers and creating a more accessible and equal society. we can't wait for you to be a part of our community!</Text>
+        <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20, width: '80%'}}>We're thrilled to have you on our journey towards a more inclusive world for people with disabilities. With your support, we can work towards breaking down barriers and creating a more accessible and equal society. we can't wait for you to be a part of our community!</Text>
         <Pressable style = {styles.button}
           onPress={ () => {setOpening(false); setFirst(true)}}
         >
@@ -156,14 +198,14 @@ const Signup = () => {
         </View>}
       {first && <View style = {{justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', alignSelf: 'center'}}>
       <Image source = {require('../Assets/images/welcome-signup.png')} style = {{width: 1000, height: 300}} resizeMode = 'contain' />
-        <Text style = {styles.headertagline}>Welcome to Alaminos city PWD community!</Text>
-        <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20}}>We're thrilled to have you on our journey towards a more inclusive world for people with disabilities. With your support, we can work towards breaking down barriers and creating a more accessible and equal society. we can't wait for you to be a part of our community!</Text>
+      <Text style = {styles.headertagline}>Let's Get Started</Text>
+        <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20, width: '80%'}}>First,  Create Your Account by Filling in Your Username and Password</Text>
         
         <Loginbox 
           placeholder = 'username' 
           secureTextEntry = {false} name = '' 
           onPress={() => console.log('usernamepressed')} 
-          onChangeText={(value) => setUsername(value)}
+          onChangeText={handleNoSpace}
           value={username}
         />
         <Loginbox 
@@ -175,110 +217,172 @@ const Signup = () => {
           value={password}
         />
         <Pressable style = {[styles.button]}
-          onPress={ () => {setFirst(false); setSecond(true)}}
+          onPress={handlefirst}
         >
           <Text style = {styles.buttontext}>Next</Text>
+        </Pressable>
+        <Pressable style = {[styles.button, {width: 100, borderColor: 'red'}]}
+          onPress={ () => {setOpening(true); setFirst(false);}}>
+         <Icon
+          name='arrow-back'
+          size={30}
+          color={'red'}
+         />
         </Pressable></View>}
           {second && 
           <View style = {{justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', alignSelf: 'center'}}>
             <Image source = {require('../Assets/images/welcome-signup.png')} style = {{width: 1000, height: 300}} resizeMode = 'contain' />
-              <Text style = {styles.headertagline}>Welcome to Alaminos city PWD community!</Text>
-              <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20}}>We're thrilled to have you on our journey towards a more inclusive world for people with disabilities. With your support, we can work towards breaking down barriers and creating a more accessible and equal society. we can't wait for you to be a part of our community!</Text>
+              <Text style = {styles.headertagline}>Then, few of your personal information</Text>
+              <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20, width: '80%'}}>The information you provided are to be displayed in your account, and rest assured that all information are not used in other means</Text>
+            <Pressable style = {[styles.button, {height: 150, width: 400, borderColor: Profilephoto === null ? LightBlue : '#202020'}]} onPress={handleProfilePhoto} disabled = {Profilephoto === null ?  false : true}>
+            <Text style = {[styles.buttontext, {fontFamily: 'Nexa-ExtraLight', color: '#707070'}]}>{Profilephoto === null ? "UPLOAD DISPLAY PHOTO" : "SUCESSFULLY UPLOADED!"}</Text>
+            <Icon
+              name = {Profilephoto === null ? 'image': 'check-box'}
+              size={100}
+              color={Profilephoto === null ? LightBlue: '#90EE90'}
+            />
+          </Pressable>
+          {Profilephoto && <Image style = {{width: 350, height: 200}} resizeMode='contain' source={{uri: Profilephoto}} />}
+        
         <Loginbox 
-          placeholder = 'FirstName' 
+          placeholder = 'First mame' 
           onChangeText={(value) => setFirstName(value)}
           value={FirstName}
         />
         <Loginbox 
-          placeholder = 'MiddleName' 
+          placeholder = 'Middle name' 
           onChangeText={(value) => setMiddleName(value)}
           value={MiddleName}
         />
         <Loginbox 
-          placeholder = 'LastName' 
+          placeholder = 'Last name' 
           onChangeText={(value) => setLastName(value)}
           value={LastName}
         />
+        
+        <Loginbox 
+          placeholder = 'Address' 
+          onChangeText={(value) => setAddress(value)}
+          value={Address}
+        />
+
         <Loginbox 
           placeholder = 'Birthday' 
           onChangeText={(value) => setBirthday(value)}
           value={Birthday}
         />
         <Loginbox 
-          placeholder = 'Sex' 
+          placeholder = 'Sex/gender' 
           onChangeText={(value) => setSex(value)}
           value={Sex}
         />
          <Pressable style = {[styles.button]}
-          onPress={ () => {setSecond(false); setthird(true)}}
+          onPress={handleSecond}
         >
           <Text style = {styles.buttontext}>Next</Text>
+        </Pressable>
+        <Pressable style = {[styles.button, {width: 100, borderColor: 'red'}]}
+          onPress={ () => {setSecond(false); setFirst(true)}}
+        >
+         <Icon
+          name='arrow-back'
+          size={30}
+          color={'red'}
+         />
         </Pressable></View>}
         {third && 
          <View style = {{justifyContent: 'center', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'}}>
        <Image source = {require('../Assets/images/welcome-signup.png')} style = {{width: 1000, height: 300}} resizeMode = 'contain' />
-        <Text style = {styles.headertagline}>Welcome to Alaminos city PWD community!</Text>
-        <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20}}>We're thrilled to have you on our journey towards a more inclusive world for people with disabilities. With your support, we can work towards breaking down barriers and creating a more accessible and equal society. we can't wait for you to be a part of our community!</Text>
+        <Text style = {styles.headertagline}>We'll keep you updated</Text>
+        <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20}}>To provide you with better service and support, we'll be collecting your phone number. Rest assured that we take your privacy seriously and will only use your phone number for the specific purposes outlined in our privacy policy.</Text>
         <Loginbox 
-          placeholder = 'MobileNumber' 
+          placeholder = 'Mobile number' 
           onChangeText={(value) => setMobileNumber(value)}
           value={MobileNumber}
         />
+        
         <Loginbox 
-          placeholder = 'Address' 
-          onChangeText={(value) => setAddress(value)}
-          value={Address}
-        />
+              placeholder = 'Alternate contact number' 
+              onChangeText={(value) => setAlternateContactNumber(value)}
+              value={AlternateContactNumber}
+            />
         <Pressable style = {[styles.button]}
         onPress={ () => {setthird(false); setForth(true)}}
       >
         <Text style = {styles.buttontext}>Next</Text>
-      </Pressable></View>}
+      </Pressable>
+      <Pressable style = {[styles.button, {width: 100, borderColor: 'red'}]}
+          onPress={ () => {setthird(false); setSecond(true);}}>
+         <Icon
+          name='arrow-back'
+          size={30}
+          color={'red'}
+         />
+        </Pressable></View>}
         {forth && 
            <View style = {{justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', alignSelf: 'center'}}>
              <Image source = {require('../Assets/images/welcome-signup.png')} style = {{width: 1000, height: 300}} resizeMode = 'contain' />
-              <Text style = {styles.headertagline}>Welcome to Alaminos city PWD community!</Text>
-              <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20}}>We're thrilled to have you on our journey towards a more inclusive world for people with disabilities. With your support, we can work towards breaking down barriers and creating a more accessible and equal society. we can't wait for you to be a part of our community!</Text>
+              <Text style = {styles.headertagline}>We'll take a look before getting you on-board</Text>
+              <Text style = {{ fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20, width: '90%'}}>We're very much excited to have you as part of Alaminos City PWD Establishment Rating app</Text>
              <Loginbox 
               placeholder = 'Nationality' 
               onChangeText={(value) => setNationality(value)}
               value={Nationality}
             /> 
             <Loginbox 
-              placeholder = 'Alternate contact number' 
-              onChangeText={(value) => setAlternateContactNumber(value)}
-              value={AlternateContactNumber}
+              placeholder = 'Type of Disability' 
+              onChangeText={(value) => setDisability(value)}
+              value={disability}
             />
             <Loginbox 
-              placeholder = 'IDType' 
-              onChangeText={(value) => setIDType(value)}
-              value={IDType}
-            />
-            <Loginbox 
-              placeholder = 'IDNumber' 
+              placeholder = 'PWD ID Number' 
               onChangeText={(value) => setIDNumber(value)}
               value={IDNumber}
             />
+            <Pressable style = {[styles.button, {height: 150, width: 400, borderColor: Idcardimage === null ? LightBlue : '#202020'}]} onPress={handleIDCardImage} disabled = {Idcardimage === null ?  false : true}>
+              <Text style = {[styles.buttontext, {fontFamily: 'Nexa-ExtraLight', color: '#707070'}]}>{Idcardimage === null ? "UPLOAD ID PHOTO" : "SUCESSFULLY UPLOADED!"}</Text>
+              <Icon
+                name = {Idcardimage === null ? 'image': 'check-box'}
+                size={100}
+                color={Idcardimage === null ? LightBlue: '#90EE90'}
+              />
+            </Pressable>
+            {Idcardimage && <Image style = {{width: 350, height: 200}} resizeMode='contain' source={{uri: Idcardimage}} />}
             <Pressable style = {[styles.button]}
-         onPress={ () => {setForth(false); setfifth(true)}}
+         onPress={handleFourth}
        >
          <Text style = {styles.buttontext}>Next</Text>
-       </Pressable></View>
+       </Pressable>
+       <Pressable style = {[styles.button, {width: 100, borderColor: 'red'}]}
+          onPress={ () => {setForth(false); setthird(true);}}>
+         <Icon
+          name='arrow-back'
+          size={30}
+          color={'red'}
+         />
+        </Pressable></View>
             }
         
-        {/* <View style={{flexDirection: "row"}}>
-        <Button  title="PROFILE PHOTO" onPress={handleProfilePhoto} />
-        <Button title="ID CARD IMAGE" onPress={handleIDCardImage} />
-        </View>
+        {fifth && 
+        <View style = {{justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', alignSelf: 'center'}}>
+        <Image source = {require('../Assets/images/welcome-signup.png')} style = {{width: 1000, height: 300}} resizeMode = 'contain' />
+          <Text style = {[styles.headertagline, {alignSelf: 'center'}]}>We'll take a look before getting you on-board</Text>
+          <Text style = {{fontSize: 20, color: White, fontFamily: 'Nexa-ExtraLight', textAlign: 'center', marginBottom: 20, width: '90%'}}>At PWD App, our goal is to empower persons with disabilities by providing a platform that promotes accessibility and inclusion. However, to ensure that the app is used by those who truly need it, we have implemented a verification process to confirm that each user is a Person with Disability (PWD).</Text>
 
-        <Pressable style = {{margin: 10, width: '35%', backgroundColor: '#f5f5f5', height: 45, justifyContent: 'center', alignItems: 'center', borderRadius: 5 }}
-        onPress={() => setNewSuperAdmin()}
-        >
-          <Text style = {{textAlign: 'center', fontSize: 17, fontWeight: '500'}}>
-             SIGN UP
-          </Text>
-        </Pressable> */}
-      </View>
+          <Pressable style = {[styles.button, {width: '85%', borderColor: LightYellow}]}
+          onPress={() => setnewuser()}
+          >
+            <Text style = {[styles.buttontext, {fontFamily: 'Nexa-Heavy', color: LightYellow}]}>Sign up</Text>
+          </Pressable>
+        <Pressable style = {[styles.button, {width: 150, borderColor: 'red'}]}
+          onPress={ () => {setForth(true); setfifth(false);}}>
+         <Icon
+          name='arrow-back'
+          size={30}
+          color={'red'}
+         />
+        </Pressable></View>}
+      </KeyboardAvoidingView>
       </ScrollView>
     </View>
   )
