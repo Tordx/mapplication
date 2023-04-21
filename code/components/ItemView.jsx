54401b24263258de710cdd698fcad6ed9fa7bf2c
Roven Kyle Narra <ellamateogang@gmail.com>
2023-04-21 17:40:56 +0800

    set 0 if math is not possible

--- code/components/ItemView.jsx
+++ code/components/ItemView.jsx
@@ -8,19 +8,20 @@ import { Modal } from 'react-native';
 
 export default function ItemView() {
 
-    const {ItemList} = useSelector((action) => action.items)
+    const { ItemList } = useSelector((action) => action.items)
     const [modal, setModal] = useState(false)
-    const dividedRating = Math.min(ItemList.Rating / ItemList.RatingCount, 5)
-    const dividedParking = Math.min(ItemList.ParkingRating / ItemList.RatingCount, 5)
-    const divideRampRating = Math.min(ItemList.RampRating / ItemList.RatingCount, 5)
-    const dividedTactilesRating =  Math.min(ItemList.TactilesRating / ItemList.RatingCount, 5)
-    const divideEstablishmentRating = Math.min(ItemList.EstablishmentRating / ItemList.RatingCount, 5)
+    
+    const dividedRating = ItemList.RatingCount === 0 ?  0 : Math.min(ItemList.Rating / ItemList.RatingCount, 5) 
+    const dividedParking = ItemList.RatingCount === 0 ?   0 : Math.min(ItemList.ParkingRating / ItemList.RatingCount, 5)
+    const divideRampRating = ItemList.RatingCount === 0 ?  0 : Math.min(ItemList.RampRating / ItemList.RatingCount, 5)
+    const dividedTactilesRating = ItemList.RatingCount === 0 ?  0 :Math.min(ItemList.TactilesRating / ItemList.RatingCount, 5)
+    const divideEstablishmentRating = ItemList.RatingCount === 0 ?  0 :Math.min(ItemList.EstablishmentRating / ItemList.RatingCount, 5) 
+    
     const [rating] = useState(dividedRating)
     const [parking] = useState(dividedParking)
     const [ramp] = useState(divideRampRating)
     const [tactiles] = useState(dividedTactilesRating)
     const [establishment] = useState(divideEstablishmentRating)
-    
 
   
 
@@ -39,7 +40,7 @@ export default function ItemView() {
         ratingColor= '#ffdd85'
         />
       
-      <Text  style={{ fontSize: 15, color: White, fontFamily: 'Nexa-ExtraLight', width: '100%', marginVertical: 10}}>Overall Rating: {Number(rating.toFixed(2))}</Text></Pressable>
+      <Text  style={{ fontSize: 15, color: White, fontFamily: 'Nexa-ExtraLight', width: '100%', marginVertical: 10}}>Overall Rating: {ItemList.RatingCount === 0 ? 0 : Number(rating.toFixed(2))}</Text></Pressable>
       </View>
       <View style = {{width: '100%', marginTop: '40%'}}>
       <Comments/>
@@ -54,7 +55,7 @@ export default function ItemView() {
           <View style = {{width: '100%', height: '100%', justifyContent: 'center', backgroundColor: Black}}>
           <Text style={{fontSize: 20, color: White, textAlign: 'center', alignSelf: 'center', fontFamily: 'Nexa-ExtraLight'}}><Text style = {{color: LightYellow, fontFamily: 'Nexa-Heavy'}}>{ItemList.Establishment}</Text> earns a total of </Text>
           <View style = {{alignSelf: 'center', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-          <Text style={{fontSize: 100, color: LightYellow, textAlign: 'center', alignSelf: 'center', fontFamily: 'Nexa-Heavy'}}>{Number(rating.toFixed(2))}</Text>
+          <Text style={{fontSize: 100, color: LightYellow, textAlign: 'center', alignSelf: 'center', fontFamily: 'Nexa-Heavy'}}>{ItemList.RatingCount === 0 ? 0 : Number(rating.toFixed(2))}</Text>
           <Text style = {{color: LightYellow, textAlign: 'center', fontFamily: 'Nexa-Heavy', fontSize: 60,}}>★</Text>
           </View>
           <Text style={{fontSize: 20, color: White, textAlign: 'center', alignSelf: 'center', fontFamily: 'Nexa-ExtraLight'}}>in <Text style = {{color: LightYellow, fontFamily: 'Nexa-Heavy'}}>{ItemList.RatingCount}</Text> reviews</Text>
@@ -62,25 +63,25 @@ export default function ItemView() {
             startingValue = {establishment}
             title = 'establishment'
             readonly
-            result = {Number(establishment.toFixed(2))}
+            result = {ItemList.establishmentRating  === 0 ? 0: Number(establishment.toFixed(2))}
             />
             <Ratings
             startingValue = {parking}
             title = 'Parking'
             readonly
-            result = {Number(parking.toFixed(2))}
+            result = {ItemList.ParkingRating === 0 ? 0: Number(parking.toFixed(2))}
             />
             <Ratings
             startingValue = {ramp}
             title = 'Ramp'
             readonly
-            result = {Number(ramp.toFixed(2))}
+            result = {ItemList.RampRating  === 0 ? 0:  Number(ramp.toFixed(2))}
             />
             <Ratings
             startingValue = {tactiles}
             title = 'Tactiles'
             readonly
-            result = {Number(tactiles.toFixed(2))}
+            result = {ItemList.TactilesRating  === 0 ? 0: Number(tactiles.toFixed(2))}
             />
           </View>
         </Modal>
