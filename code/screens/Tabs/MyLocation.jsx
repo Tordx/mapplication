c9bda04f888d5f0032c4b91a84ff4abd1f553687
Roven Kyle Narra <ellamateogang@gmail.com>
2023-04-24 00:15:38 +0800

    finally fixed the error on location uses perm api

--- code/screens/Tabs/MyLocation.jsx
+++ code/screens/Tabs/MyLocation.jsx
@@ -1,32 +1,81 @@
 import React, { useState, useEffect } from "react";
-import MapboxGL, { MarkerView } from "@rnmapbox/maps";
-import Geolocation from '@react-native-community/geolocation';
-
+import MapboxGL from "@rnmapbox/maps";
+import { check, PERMISSIONS, request } from 'react-native-permissions';
+import Geolocation from "@react-native-community/geolocation";
+import { Pressable, Text, Linking, Platform, StyleSheet } from "react-native";
+import { White, LightYellow, LightBlue, Black } from "../../Assets/Colors/Colors";
+import  Icon  from "react-native-vector-icons/MaterialIcons";
 
 const MyLocation = () => {
 
-  const [userLocation, setUserLocation] = useState(null);
+  const [visible, setVisible] = useState(false);
+  const [showbutton, setButton] = useState(true);
+
+  const openLocation = () => {
+    
+    setButton(false)
+    if (Platform.OS === 'android') {
+      Linking.openSettings();
+      setTimeout(() => {
+        Linking.sendIntent('android.settings.LOCATION_SOURCE_SETTINGS');
+      }, 1000);
+      
+    }
+  }
+  
+  const requestLocationPermission = async () => {
+    const status = await check(PERMISSIONS.ANDROID.LOCATION_WHEN_IN_USE);
+    if (status === 'granted') {
+      setVisible(true);
+      setButton(false)
+      console.log(result);
+    } else {
+      const result = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      if (result === 'granted') {
+        setVisible(true);
+      }
+    }
+  };
 
   useEffect(() => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        setUserLocation([position.coords.longitude, position.coords.latitude]);
-      },
-      (error) => console.log(error),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-  }, [userLocation]);
+    requestLocationPermission
+  },[])
 
   return (
-    <MapboxGL.MapView style={{ flex: 1 }}
-    logoEnabled = {false}
-    attributionEnabled = {false}
-    >
-      <MapboxGL.Camera
-       zoomLevel={14} centerCoordinate = {userLocation} 
-      />
-      <MapboxGL.UserLocation visible={true} />
-    </MapboxGL.MapView>
+    <>
+    
+      <MapboxGL.MapView style={{ flex: 1 }} logoEnabled={false} attributionEnabled={false}>
+        <MapboxGL.Camera
+          zoomLevel={17}
+          followUserLocation={true}
+          followUserMode="normal"
+          followZoomLevel={17}
+        />
+      <MapboxGL.UserLocation visible={true} showsUserHeadingIndicator = {true} minDisplacement={10} />
+      </MapboxGL.MapView>
+      {showbutton ? <Pressable style = {[styles.button, {width: '50%', borderColor: Black, flexDirection: 'row', height: 50, position: 'absolute', top: 0, justifyContent: 'center', alignSelf: 'center'}]}  onPress={openLocation} >
+      <Text style = {[styles.buttontext, {fontFamily: 'Nexa-Heavy', color: Black, fontSize: 17}]}>Set Location</Text>
+      </Pressable> : null}
+      {/* <Pressable style = {[styles.button, {width: '100%', borderWidth: 0, flexDirection: 'row', height: 75, position: 'absolute', bottom: 100, left: 10, justifyContent: 'center', alignSelf: 'center'}]}  onPress={openLocation} >
+      <Icon name = 'person-pin' size = {75} color={'red'} />
+      <Text style = {{fontFamily: 'Nexa-Heavy', color: Black, fontSize: 17, width: '90%'}}>Where you at</Text>
+      </Pressable>  */}
+    </>
   );
 };
-export default MyLocation
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 20, 
+    height: 60, 
+    width: '50%', 
+    borderWidth: 1, 
+    borderColor: White,  
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    margin: 5,
+    marginTop: 20
+},
+})
+
+export default MyLocation;
