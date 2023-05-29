6cc7fe14c0e8207a802c71b701568a9ffe29df8c
Roven Kyle Narra <ellamateogang@gmail.com>
2023-05-29 21:46:49 +0800

    data pulling changed

--- code/screens/UserViewPage.jsx
+++ code/screens/UserViewPage.jsx
@@ -16,7 +16,6 @@ export default function UserViewPage() {
     const [nationality, setNationality] = useState('')
     const navigation = useNavigation();
     const colorScheme = useColorScheme()=== 'dark';
-    console.log(userview)
     const getdata = async () => {
       try {
         let result = await dbremoteAccounts.allDocs({
@@ -29,7 +28,8 @@ export default function UserViewPage() {
             item => item.doc
           );
           let filteredData = modifiedArr.filter(item => {
-            return item.UserID === userview.UserID
+            
+            return item.username === userview.username
           });
           if (filteredData.length) {
             let newFilterData = filteredData.map((item) => {
@@ -59,13 +59,13 @@ export default function UserViewPage() {
            <View style={{borderWidth: 1, borderStyle: 'dashed', borderColor: LightYellow, alignItems: 'center', justifyContent: 'center', width: 220, height: 220, borderRadius: 200, marginBottom: 20}}>
      <View style={{borderWidth: 1, borderColor: LightBlue, alignItems: 'center', justifyContent: 'center', width: 210, height: 210, borderRadius: 200}}>
       <Image
-        source={{ uri: data.Image}}
+        source={{ uri: data.Image || userview.Image}}
         style={{width: 200, height: 200, borderRadius: 200,}}
       />
       </View>
       </View>
         <View style={{alignItems: 'center', justifyContent: 'center', flexDirection: 'column', margin: 5}}>
-          <Text style={{ fontSize: 30, color:  colorScheme ? White : Black, fontFamily: 'Nexa-Heavy'}}>{data.FullName}</Text>
+          <Text style={{ fontSize: 30, color:  colorScheme ? White : Black, fontFamily: 'Nexa-Heavy'}}>{userview.FullName}</Text>
           <View style = {{alignItems: 'center', justifyContent: 'center', flexDirection: 'row', marginTop: 15}}>
               <FontAwesome size={30} name = 'star-o' color = {LightYellow}/>
               <Text style={{marginLeft: 15, fontSize: 25, textAlign: 'center', fontFamily: 'Nexa-ExtraLight', color:  colorScheme ? White : Black}}>{data.CommentCount} REVIEWS</Text>
@@ -73,26 +73,26 @@ export default function UserViewPage() {
         </View>
         <Text style={{ fontSize: 25, color:  colorScheme ? White : Black, fontFamily: 'Nexa-Heavy', width: '90%', textAlign: 'left'}}>About</Text>
     <View style={{alignItems: 'center', justifyContent: 'center', flexDirection: 'row', margin: 5, width: '95%', height: 90, borderWidth: 1, borderColor: LightBlue,borderRadius: 20}}>
-    <View style={{alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row', width: '90%', height: 90}}>
-      <View style = {{alignItems: 'center', justifyContent: 'center', width: '30%', flexDirection: 'column', height: '100%'}}>
-          <FontAwesome
-            name  = {data.Sex === 'female' ? 'venus' : 'mars'}
-            color = {data.Sex === 'female' ? 'pink' : 'blue'}
-            size={25}
-            style = {{ position: 'absolute', bottom: 40}}
-          />
-          <Text style={{fontSize: 15, fontFamily: 'Nexa-ExtraLight', color:  colorScheme ? White : Black, marginLeft: 10,  position: 'absolute', bottom: 20}}>{data.Sex}</Text>
+      <View style={{alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row', width: '90%', height: 90}}>
+        <View style = {{alignItems: 'center', justifyContent: 'center', width: '30%', flexDirection: 'column', height: '100%'}}>
+            <FontAwesome
+              name  = {data.Sex === 'female' ? 'venus' : 'mars'}
+              color = {data.Sex === 'female' ? 'pink' : 'blue'}
+              size={25}
+              style = {{ position: 'absolute', bottom: 40}}
+            />
+            <Text style={{fontSize: 15, fontFamily: 'Nexa-ExtraLight', color:  colorScheme ? White : Black, marginLeft: 10,  position: 'absolute', bottom: 20}}>{data.Sex}</Text>
+          </View>
+          <View style = {{alignItems: 'center', justifyContent: 'center', width: '33%',height: '100%'}}>
+          <FontAwesome5 size={25} name = 'wheelchair' color = {'blue'}  style = {{ position: 'absolute', bottom: 40}}/>
+                <Text style={{ position: 'absolute', bottom: 20, fontSize: 15, textAlign: 'center', fontFamily: 'Nexa-ExtraLight', color:  colorScheme ? White : Black}}>{data.Disability}</Text>
+              </View>
+              <View style = {{alignItems: 'center', justifyContent: 'center', width: '30%', height: '100%'}}>
+            <CountryFlag isoCode={nationality} size={20} style = {{bottom: 43, position: 'absolute'}} />
+            <Text style={{fontSize: 15, fontFamily: 'Nexa-ExtraLight', color:  colorScheme ? White : Black,  position: 'absolute', bottom: 20}}>{nationality.toUpperCase()}</Text>
+          </View>
         </View>
-        <View style = {{alignItems: 'center', justifyContent: 'center', width: '33%',height: '100%'}}>
-        <FontAwesome5 size={25} name = 'wheelchair' color = {'blue'}  style = {{ position: 'absolute', bottom: 40}}/>
-              <Text style={{ position: 'absolute', bottom: 20, fontSize: 15, textAlign: 'center', fontFamily: 'Nexa-ExtraLight', color:  colorScheme ? White : Black}}>{data.Disability}</Text>
-            </View>
-            <View style = {{alignItems: 'center', justifyContent: 'center', width: '30%', height: '100%'}}>
-          <CountryFlag isoCode={nationality} size={20} style = {{bottom: 43, position: 'absolute'}} />
-          <Text style={{fontSize: 15, fontFamily: 'Nexa-ExtraLight', color:  colorScheme ? White : Black,  position: 'absolute', bottom: 20}}>{nationality.toUpperCase()}</Text>
-        </View>
-         </View>
-         </View>
+      </View>
       <Pressable style = {{position: 'absolute', top: 15, left: 15}}
         onPress={() => navigation.goBack('CommentTab')}
       >
